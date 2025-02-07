bbf8d212f173870c6c6c306ebbe7d3787ce87cea
David Rojas <dmrojassantana@gmail.com>
2025-02-07 21:59:20 +0100

    Implementar registro de usuario con validación de nombre, email y contraseña

--- api/controllers/sessions.controller.js
+++ api/controllers/sessions.controller.js
@@ -2,21 +2,42 @@ const Session = require("../models/session.model");
 const User = require("../models/user.model");
 const createError = require("http-errors");
 
-module.exports.create = (req, res, next) => {
+module.exports.create = async (req, res, next) => {
   const { email, password } = req.body;
 
   // 1. find user by email
+  const user = await User.findOne({ email });
+  if (!user) {
+    return next(createError(401, "Credenciales inválidas"));
+  }
+
   // 2. check password
+  const isPasswordValid = await user.comparePassword(password);
+  if (!isPasswordValid) {
+    return next(createError(401, "Credenciales inválidas"));
+  }
+
   // 3. create session
+  const session = new Session({ userId: user._id });
+  await session.save();
+
   // 4. send session id in a cookie
+  res.header("Set-Cookie", `session_id=${session._id}; HttpOnly`);
 
-  res.header("Set-Cookie", "session_id=12345");
-
-  res.json({ message: "TO DO!" });
+  res.json({ message: "Inicio de sesión exitoso" });
 };
 
 module.exports.destroy = (req, res, next) => {
   // access current request session. remove and send 204 status
-
-  res.status(204).send();
+  if (req.session) {
+    req.session.destroy(err => {
+      if (err) {
+        return next(err);
+      }
+      res.clearCookie("session_id");
+      res.status(204).send();
+    });
+  } else {
+    res.status(204).send();
+  }
 };
--- api/controllers/users.controller.js
+++ api/controllers/users.controller.js
@@ -1,11 +1,38 @@
 const createError = require("http-errors");
 const User = require("../models/user.model");
 
-module.exports.create = (req, res, next) => {
-  res.json({ message: "TO DO!" });
+module.exports.create = async (req, res, next) => {
+  try {
+    const { name, email, password } = req.body;
+
+    // Validar que se proporcionen name, email y password
+    if (!name || !email || !password) {
+      throw createError(400, "Name, email y password son requeridos");
+    }
+
+    // Crear un nuevo usuario
+    const newUser = new User({ name, email, password });
+    await newUser.save();
+
+    res.status(201).json({ message: "Usuario creado exitosamente" });
+  } catch (error) {
+    next(error);
+  }
 };
+
 
 module.exports.profile = (req, res, next) => {
   // access current request user
-  res.json({ message: "TO DO!" });
+  if (!req.session || !req.session.userId) {
+    return res.status(401).json({ message: "No autorizado" });
+  }
+
+  User.findById(req.session.userId)
+    .then(user => {
+      if (!user) {
+        return res.status(404).json({ message: "Usuario no encontrado" });
+      }
+      res.json({ email: user.email }); // Retornar solo el email por razones de seguridad
+    })
+    .catch(err => next(err));
 };
